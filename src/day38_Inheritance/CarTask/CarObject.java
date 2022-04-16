08f649b24077a4869f66ade76cf29fb5f6953dd8
arlind00 <aarlin@live.com>
2022-04-16 14:42:39 +0200

    day38: Inheritance continue - Method Overriding

--- src/day38_Inheritance/CarTask/CarObject.java
+++ src/day38_Inheritance/CarTask/CarObject.java
@@ -1,26 +1,43 @@
 package day38_Inheritance.CarTask;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CarObject {
 
 
     public static void main(String[] args) {
 
 
-
-        Car car1 = new Car("Toyota", "Camry", 2020, 25000, "grey", 5000);
-        Car car2 = new Car("Tesla", "model S", 2019, 35000, "black", 20000);
-        Car car3 = new Car("BMW", "S2", 2008, 15000, "white", 70000);
-
+        Toyota toyota = new Toyota( "Camry", 2020, 25000, "grey", 5000);
+        Tesla tesla = new Tesla( "model S", 2019, 35000, "black", 20000);
+        BMW bmw = new BMW( "S2", 2008, 15000, "white", 70000);
 
 
 
+        System.out.println("car1 = " + toyota);
+        System.out.println("car2 = " + tesla);
+        System.out.println("car3 = " + bmw);
 
 
+        toyota.drive();
+        toyota.start();
+        toyota.reliable();
+        System.out.println();
 
+        tesla.drive();
+        tesla.start();
+        tesla.autoPilot();
+        System.out.println();
 
+        bmw.drive();
+        bmw.start();
+        bmw.breaksDown();
+        bmw.racing();
+        System.out.println();
 
 
 
     }
-    }
+}
 
