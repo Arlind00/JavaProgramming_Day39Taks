7b1cd32d69f1bbee38f0cf5680a2ba5b4fd04408
arlind00 <aarlin@live.com>
2022-04-16 14:35:01 +0200

    day38: Inheritance continue - Method Overriding

--- src/day38_Inheritance/CarTask/CarObject.java
+++ src/day38_Inheritance/CarTask/CarObject.java
@@ -3,4 +3,24 @@ package day38_Inheritance.CarTask;
 public class CarObject {
 
 
-}
+    public static void main(String[] args) {
+
+
+
+        Car car1 = new Car("Toyota", "Camry", 2020, 25000, "grey", 5000);
+        Car car2 = new Car("Tesla", "model S", 2019, 35000, "black", 20000);
+        Car car3 = new Car("BMW", "S2", 2008, 15000, "white", 70000);
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+
