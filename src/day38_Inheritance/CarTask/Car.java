9b5263c4698408d8ce781c3f730b288315691a77
arlind00 <aarlin@live.com>
2022-04-16 11:53:20 +0200

    day38: Inheritance continue - Method Overriding

--- src/day38_Inheritance/CarTask/Car.java
+++ src/day38_Inheritance/CarTask/Car.java
@@ -28,6 +28,17 @@ public class Car {
         System.out.println(brand + " " + model + " is driving");
     }
 
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
 }
 
 /*
