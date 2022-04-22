25aaa75e55242de465f4b82bad439112c5a8c4cd
arlind00 <aarlin@live.com>
2022-04-22 15:55:44 +0200

    day40_Exceptions

--- src/day41_Exceptions/TryCatchBlocks.java
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -8,15 +8,12 @@ public class TryCatchBlocks {
         System.out.println("Test1 started");
 
         try {
-
             System.out.println(9 / 0);
             System.out.println("Try Block");
 
         } catch (ArithmeticException e) {
-
             System.out.println("Catch Block");
-            System.out.println("Arithmetic Exception has occurred");
-
+            System.out.println("Arithmetic Exception has occurred");e.printStackTrace();
         }
 
         System.out.println("Test1 Completed");
@@ -26,11 +23,9 @@ public class TryCatchBlocks {
 
 
         System.out.println("Test2 started");
-
         int[] numbers = {1,2,3,4,5};
 
         try {
-
             System.out.println(numbers[200]);
             System.out.println("Try Block");
 
@@ -40,9 +35,7 @@ public class TryCatchBlocks {
            // System.out.println("runtime exception has occurred");
             e.printStackTrace();
             // System.out.println( e.getMessage() );
-
         }
-
         System.out.println("Test2 Completed");
 
 
@@ -51,21 +44,32 @@ public class TryCatchBlocks {
 
         System.out.println("Test3 started");
 
-
         try {
-
             System.out.println("Cydeo".substring(2, 0));
 
         }catch (RuntimeException e){
-
             e.printStackTrace();
         }
 
-        System.out.println("Test3 started");
+        System.out.println("Test3 completed");
 
 
         System.out.println("--------------------------------------------------");
 
+
+        System.out.println("Hello");
+
+        try {
+            Thread.sleep(3000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Cydeo");
+
+
+        System.out.println("------------------------------------------");
 
 
 
