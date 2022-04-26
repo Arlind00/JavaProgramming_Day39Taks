a93369a3252e93d2c158e1f257448f6477a7b176
arlind00 <aarlin@live.com>
2022-04-26 12:23:36 +0200

    day43_Abstraction

--- src/day43_Abstraction/employee/Developer.java
+++ src/day43_Abstraction/employee/Developer.java
@@ -1,4 +1,24 @@
 package day43_Abstraction.employee;
 
-public class Developer {
+public final class Developer extends Employee {
+
+
+    public Developer(String name, int age, char gender, int id, double salary) {
+        super(name, age, gender, id, "Developer ", salary);
+    }
+
+
+    public void work() {
+        System.out.println(getName()+" is developing application");
+    }
+
+    public void sleep() {
+        System.out.println(getName()+" sleeps 6 hours");
+    }
+
+    public void unitTest(){
+        System.out.println(getName()+" is unit testing");
+    }
+
+
 }
--- src/day43_Abstraction/employee/Teacher.java
+++ src/day43_Abstraction/employee/Teacher.java
@@ -1,4 +1,18 @@
 package day43_Abstraction.employee;
 
-public class Teacher {
+public final class Teacher extends Employee {
+
+    public Teacher(String name, int age, char gender, int id,  double salary) {
+        super(name, age, gender, id, " Teacher", salary);
+    }
+
+
+    public void work() {
+        System.out.println(getName()+" is teaching.");
+    }
+
+
+    public void sleep() {
+        System.out.println(getName()+" is slepping 8 hours.");
+    }
 }
