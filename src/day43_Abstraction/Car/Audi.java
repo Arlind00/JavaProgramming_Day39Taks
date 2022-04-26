5adc1d313ad16748598f889f3d5d7117a0f6ab28
arlind00 <aarlin@live.com>
2022-04-26 11:25:26 +0200

    day43_Abstraction

--- src/day43_Abstraction/Car/Audi.java
+++ src/day43_Abstraction/Car/Audi.java
@@ -1,6 +1,21 @@
 package day43_Abstraction.Car;
 
-public class Audi {
+public final class Audi extends Car {
+
+
+    public Audi( String model, String color, int year, double price) {
+        super("Audi", model, color, year, price);
+    }
+
+
+    public void start() {
+        System.out.println("Push the start button");
+    }
+
+    public void autoPark(){
+        System.out.println(getBrand()+" "+getModel()+" has auto park feature");
+    }
+
 
 
 }
