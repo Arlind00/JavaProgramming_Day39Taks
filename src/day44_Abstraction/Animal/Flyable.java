5ee1b3a9ebd63e3525869612810ab4aab804a653
arlind00 <aarlind@live.com>
2022-05-08 11:08:02 +0200

    day44_Abstraction

---
+++ src/day44_Abstraction/Animal/Flyable.java
@@ -0,0 +1,10 @@
+package day44_Abstraction.Animal;
+
+public interface Flyable {
+
+    boolean canFly = true;      // final and static ( by default)
+
+    void fly();                 // abstract by default, public default
+
+
+}
