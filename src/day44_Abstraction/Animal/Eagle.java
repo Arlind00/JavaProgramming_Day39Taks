5b64254ab0669906a83b391d1d462490d614767c
arlind00 <aarlind@live.com>
2022-05-08 11:10:55 +0200

    day44_Abstraction

--- src/day44_Abstraction/Animal/Eagle.java
+++ src/day44_Abstraction/Animal/Eagle.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.Animal;
 
-public final class Eagle extends Animal implements WildAnimal{
+public final class Eagle extends Animal implements WildAnimal,Flyable{
 
     public Eagle(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -15,4 +15,12 @@ public final class Eagle extends Animal implements WildAnimal{
     public void hunt() {
         System.out.println(getName() + "hunts snakes");
     }
+
+
+    @Override
+    public void fly() {
+        System.out.println(getName() + "fly till 250 kmh");
+    }
+
+
 }
