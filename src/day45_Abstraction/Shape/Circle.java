dd741fb369d5ee1b9951f13722ab1f3362791b3d
arlind00 <aarlind@live.com>
2022-05-17 11:06:17 +0200

    day45_Abstraction-task

---
+++ src/day45_Abstraction/Shape/Circle.java
@@ -0,0 +1,45 @@
+package day45_Abstraction.Shape;
+
+public class Circle extends Shape {
+
+
+    private double radius;
+
+    public final static double pi = 3.1428 ;
+
+    public Circle(double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+
+        if(radius<0){
+            throw new RuntimeException("Invalid value" + radius );
+        }
+        this.radius = radius;
+    }
+
+
+
+    public double area() {
+        return (radius * radius * pi);
+    }
+
+    public double perimeter() {
+        return (2 * radius * pi);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
