4375f361842bb9502aa8a2f1f6d1ff91bc17f4ef
arlind00 <aarlind@live.com>
2022-05-17 11:34:15 +0200

    day45_Abstraction_T 4th commit

--- src/day45_Abstraction/Shape/Cube.java
+++ src/day45_Abstraction/Shape/Cube.java
@@ -1,4 +1,42 @@
 package day45_Abstraction.Shape;
 
-public class Cube {
+
+public class Cube extends Shape {
+
+
+    private double edge;
+
+
+    public Cube(String name, double edge) {
+        super("Cube");
+        setEdge(edge);
+    }
+
+    public double getEdge() {
+        return edge;
+    }
+
+    public void setEdge(double edge) {
+
+        if (edge <= 0) {
+            throw new RuntimeException("Invalid value: " + edge);
+        }
+        this.edge = edge;
+    }
+
+    public double area() {
+        return (6 * edge * edge);
+    }
+
+    public double perimeter() {
+        return (12 * edge);
+    }
+
+
+    public String toString() {
+        return " Cube {" +
+                super.toString() +
+                " edge= " + edge +
+                '}';
+    }
 }
--- src/day45_Abstraction/Shape/Cylinder.java
+++ src/day45_Abstraction/Shape/Cylinder.java
@@ -1,4 +1,54 @@
 package day45_Abstraction.Shape;
 
-public class Cylinder {
+public class Cylinder extends Shape{
+
+
+    private double h;
+    private double r;
+    private final double pi = 3.14;
+
+    public Cylinder(double h, double r) {
+        super("Cylinder");
+        setH(h);
+        setR(r);
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        if (h <= 0){
+            throw new RuntimeException("Invalid value: " + h);
+        }
+        this.h = h;
+    }
+
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        if (r <= 0){
+            throw new RuntimeException("Invalid value: " + r);
+        }
+        this.r = r;
+    }
+
+    public double getPi() {
+        return pi;
+    }
+
+
+
+    public double area() {
+        return ((2 * pi * r * h) + (2 * pi * r * r));
+    }
+
+    public double perimeter() {
+        return ( 2 * pi * r);
+    }
+
+
 }
