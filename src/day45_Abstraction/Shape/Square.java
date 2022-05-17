a2c01b6ee73071c82a0d8f6a4c22b07716ad6099
arlind00 <aarlind@live.com>
2022-05-17 11:17:20 +0200

    day45_Abstraction_T 3rd commit

--- src/day45_Abstraction/Shape/Square.java
+++ src/day45_Abstraction/Shape/Square.java
@@ -1,4 +1,44 @@
 package day45_Abstraction.Shape;
 
-public class Square {
+public class Square extends Shape {
+
+
+    private double side;
+
+
+    public Square(double side) {
+        super("Square");
+        setSide(side);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+
+        if (side <= 0){
+            throw new RuntimeException("Invalid vaule: " + side);
+        }
+        this.side = side;
+    }
+
+
+
+    public double area() {
+        return (side * side);
+    }
+
+
+    public double perimeter() {
+        return (4 * side);
+    }
+
+
+    public String toString() {
+        return "Square{" +
+                super.toString() +
+                " side= " + side +
+                '}';
+    }
 }
